7e430ad591847f3dedb883caf066e679fdb417a9
Satyam <satyam.singh@simformsolutions.com>
2026-06-22 13:08:11 +0530

    refactor:mplement route in scalable way

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { useAuth } from "@/hook/useAuth"
+import { Navigate, Outlet } from "react-router";
+
+export const ProtectedRoute = () => {
+    const {isAuthenticated}=useAuth();
+    console.log(isAuthenticated);
+    
+
+  return (
+    <>
+        {
+            isAuthenticated?<Outlet/>:<Navigate to='/' replace/>
+        }
+    </>
+  )
+}
